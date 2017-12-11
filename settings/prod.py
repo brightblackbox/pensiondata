7a44cd33be24bbd98a890e92b3ca53050272ed40
Nick Veith <=>
2017-12-10 20:39:29 -0700

    Convert to heroku postgres

--- settings/prod.py
+++ settings/prod.py
@@ -4,3 +4,7 @@ from .base import *
 DEBUG = False
 ALLOWED_HOSTS = ['*']
 
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
