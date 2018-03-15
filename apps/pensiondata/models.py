b9fdd4de7541b860a0a2e1dc7832487375c4842b
andrey.bogdanov <andrey.bogdanov@keyua.com>
2018-03-15 10:39:41 +0200

    #58 Type and State Names to dropdown government in Plan model

--- apps/pensiondata/models.py
+++ apps/pensiondata/models.py
@@ -114,7 +114,7 @@ class Government(models.Model):
         verbose_name_plural = 'Governments'
 
     def __str__(self):
-        return self.name
+        return "%s %s, %s" % (self.name, self.government_type, self.state)
 
 
 class GovernmentAttribute(models.Model):
